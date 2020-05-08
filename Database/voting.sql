69280174f872e5ce5beef26644ad74305678aad8
Jainam12 <jainamdoshi1210@gmail.com>
2020-05-09 00:02:09 +0530

    Update voting.sql

--- Database/voting.sql
+++ Database/voting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2020 at 06:21 PM
+-- Generation Time: May 08, 2020 at 08:31 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -34,6 +34,15 @@ CREATE TABLE `candidates` (
   `can_id` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `candidates`
+--
+
+INSERT INTO `candidates` (`id`, `name`, `can_id`) VALUES
+(1, 'Tony Stark', 'TS01'),
+(2, 'Natasha Romanoff', 'NR05'),
+(3, 'Steve Rogers', 'SR00');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +55,15 @@ CREATE TABLE `voters` (
   `pass` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `voters`
+--
+
+INSERT INTO `voters` (`id`, `uname`, `pass`) VALUES
+(1, 'Rudresh', '123'),
+(2, 'Jainam', '123'),
+(3, 'Nirali', '123');
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +75,23 @@ CREATE TABLE `votes` (
   `uname` varchar(20) NOT NULL,
   `can_id` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`id`, `uname`, `can_id`) VALUES
+(1, 'Rudresh', 'TS01'),
+(2, 'Rudresh', 'NR05'),
+(3, 'Rudresh', 'NR05'),
+(4, 'Rudresh', 'SR00'),
+(5, 'Rudresh', 'SR00'),
+(6, 'Rudresh', 'NR05'),
+(7, 'Rudresh', 'NR05'),
+(8, 'Rudresh', 'TS01'),
+(9, 'Rudresh', 'TS01'),
+(10, 'Rudresh', 'TS01'),
+(11, 'Rudresh', 'NR05');
 
 --
 -- Indexes for dumped tables
@@ -88,19 +123,19 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
